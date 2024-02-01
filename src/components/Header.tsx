e0750ac6511b88c9586896bee934268084fd0ab6
dmitriy_krasikov <krasikov.d@claramente.ru>
2024-02-01 19:20:49 +0300

    ))

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,19 @@
 function Header () {
     return (
-        <header>
-            KRASIKOV
-            <nav>
-                <a href="">О себе</a>
-                <a href="">Блог</a>
-                <a href="">Контакты</a>
-            </nav>
+        <header className='header'>
+            KRASIKOV DMITRIY
+            <div>
+                <nav>
+                    <a className='header__link' href="">О себе</a>
+                    <a className='header__link' href="">Блог</a>
+                    <a className='header__link' href="">Контакты</a>
+                </nav>
+                <div className='header__language'>
+
+                </div>
+            </div>
         </header>
     )
 }
+
 export default Header;
